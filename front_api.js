a0918669f51574b08cf4c714c74c7cb24639b315
tobeapro <471091127@qq.com>
2019-06-05 21:59:43 +0800

    更新api

--- front_api.js
+++ front_api.js
@@ -21,8 +21,66 @@ router.post('/front_manage/api/getArticles',(req,res,next)=>{
 		}
 	})
 })
+// 获取最近文章
+router.post('/front_manage/api/latestArticles',(req,res,next)=>{
+	const num = typeof(req.body.num)==='number'?req.body.num:5
+	models.article.find()
+	.sort({ create_time: -1 })
+	.select('title')
+	.limit(num)
+	.exec((err,data)=>{
+		if(err){
+			return res.send(err)
+		}
+		return res.send({
+			result:1,
+			data
+		})
+	})
+})
+router.post('/front_manage/api/articleCount',(req,res,next)=>{
+	models.article.find()
+	.select('classify')
+	.exec((err,data)=>{
+		if(err){
+			return res.send(err)
+		}
+		const total = data.length
+		let groupObj = {}
+		data.forEach(item=>{
+			const classify = item.classify
+			let classifyAry = classify.split(',')
+			classifyAry.forEach(key=>{
+				if(groupObj[key]){
+					groupObj[key]++
+				}else{
+					groupObj[key] = 1
+				}
+			})
+		})
+		let groups = [] 
+		Object.keys(groupObj).forEach(key=>{
+			groups.push({
+				item:key,
+				count:groupObj[key]
+			})
+		})
+		res.send({
+			result:1,
+			data:{
+				total,
+				groups
+			}
+		})
+
+	})
+})
 // 获取文章详情
 router.post('/front_manage/api/airticleDetail',(req,res,next)=>{
+	const objIdReg = /^[a-z0-9]{24}$/;
+	if(!objIdReg.test(req.body.id)){
+		return res.send({result:2,msg:'文章不存在'})
+	}
 	models.article.findById(req.body.id,(err,data)=>{
 		if(err){
 			return res.send(err)
