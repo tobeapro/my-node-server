019eed1f49b8f03ba3f42edc440a6d4c6f772b5a
tobeapro <471091127@qq.com>
2020-06-16 22:03:22 +0800

    :pushpin: conf: mongoose config

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/backSystem',{ useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/backSystem',{ useNewUrlParser: true, useUnifiedTopology:true })
 const db = mongoose.connection
 db.on('error', () => {
   console.log('database connect error')
